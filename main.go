d4c2e9ab75e6bed74030526730a773b03046d44c
Masataka Shida <ar.tec.masa@gmail.com>
2020-05-16 23:13:45 +0900

    Add Setting struct

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Setting struct {
+	ContainerName string   `json:"container_name"`
+	RebuildCmds   []string `json:"rebuild_cmds"`
+	TestLists     []string `json:"test_lists"`
+}
 
 func main() {
 	fmt.Println("Hello World!")
