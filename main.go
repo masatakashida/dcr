4f44afaeebe4f5b40e05f4f356ce8c895f98284c
Masataka Shida <ar.tec.masa@gmail.com>
2020-05-16 23:15:45 +0900

    Create loadSettingFile func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -12,4 +13,18 @@ type Setting struct {
 
 func main() {
 	fmt.Println("Hello World!")
+	loadSettingFile()
+}
+
+func loadSettingFile() {
+	// TODO(shida): 設定ファイルから読み込むようにする。
+	b := []byte(`{"container_name": "web", "rebuild_cmds": ["down", "build", "up -d"], "test_lists":["rspec", "rails test", "rubocop"]}`)
+	var s Setting
+	if err := json.Unmarshal(b, &s); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("container_name:", s.ContainerName)
+	fmt.Println("rebuild_cmds:", s.RebuildCmds)
+	fmt.Println("test_lists:", s.TestLists)
 }
